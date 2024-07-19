01074b8a12894825ffeaf6a09f5857b3ad4342ee
ghenriqu3 <ghsousa.silva@gmail.com>
2024-07-19 20:46:12 -0300

    adding new slider

--- src/componentes/GalleryHomePage/GalleryHomePage.jsx
+++ src/componentes/GalleryHomePage/GalleryHomePage.jsx
@@ -1,47 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './GalleryHomePage.css'
+import { useState } from 'react'
 import ImageCarousel1 from '../../assets/img/home-slide-1.jpeg'
 import ImageCarousel2 from '../../assets/img/home-slide-2.jpeg'
 import ImageCarousel3 from '../../assets/img/home-slide-3.jpeg'
 import ImageCarousel4 from '../../assets/img/home-slide-4.jpeg'
 import ImageCarousel5 from '../../assets/img/home-slide-5.jpeg'
 import ImageCarousel6 from '../../assets/img/home-slide-6.jpeg'
-
-
-function GalleryHomePage() {
+function GalleryHomePage({banners}) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  
+  const goNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length)
+  }
+  const goPrev = () => {
+    setCurrentIndex((prevIndex) =>{
+      prevIndex === 0 ? banners.length -1 : prevIndex - 1
+    })
+  }
   return (
-    <div>
-            <div id="carouselExample" className="carousel slide">
-  <div className="carousel-inner h-50">
-    <div className="carousel-item active ">
-      <img src={ImageCarousel1} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={ImageCarousel2} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={ImageCarousel3} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={ImageCarousel4} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={ImageCarousel5} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={ImageCarousel6} className="d-block w-100 " alt="..." />
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-
+    <div className='carousel'>
+      <button onClick={goPrev} className='carousel-button prev-button'>&lt;</button>
+      <div>
+        <img src={banners[currentIndex]} alt="banner" className='carousel-image'/>
+        {console.log(banners[currentIndex])}
+      </div>
+      <button onClick={goNext} className='carousel-button next-button'>&gt;</button>
     </div>
     )
 }
