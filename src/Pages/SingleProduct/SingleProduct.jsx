d5a031ee2fd7f4511501690f3e495ad3a075ad65
Visitante <visitante.digital@digitalcollegesul.local>
2024-07-23 13:16:18 -0300

    adding newPage

---
+++ src/Pages/SingleProduct/SingleProduct.jsx
@@ -0,0 +1,17 @@
+import Header from "../../componentes/Header/Header";
+import {Footer}  from "../../componentes/Footer/Footer";
+import { ProductId } from "../../componentes/Product_ID/ProductId";
+
+export const SingleProduct = () =>{
+    return(
+        <div>
+            <Header />
+            <ProductId />
+
+            <h1>teste</h1>
+            <Footer />
+        </div>
+    )
+}
+
+
---
+++ src/componentes/Product_ID/ProductId.jsx
@@ -0,0 +1,24 @@
+
+import imgTenis from "../../assets/img/image2.png"
+
+export const ProductId = () => {
+    return (
+        <div>
+            <img src={imgTenis} alt="" />
+            <h1> Tênis Nike Revolution 6 Next Nature Masculino</h1>
+
+
+            <span>Descrição do Produto</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi numquam praesentium architecto iste rem corporis reiciendis amet pariatur quaerat exercitationem labore quia quasi laboriosam, laborum natus dolor eum vitae.</p>
+
+
+            
+
+            <button>Comprar</button>
+
+
+        </div>
+    )
+}
+
+ 
