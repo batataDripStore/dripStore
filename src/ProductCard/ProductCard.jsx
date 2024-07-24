0e27509946539f46f1787981f5a669381bc18c3e
Joyce Silva <joycejsm96@protonmail.com>
2024-07-24 16:21:21 -0300

    conforme pedido, alterei o productcard para receber as props de um array. Percorri o array com um map para que devolvesse os valore. Fiz ajustes no css pois após essa mudança, os cards apareceram tds em coluna

--- src/ProductCard/ProductCard.jsx
+++ src/ProductCard/ProductCard.jsx
@@ -1,44 +1,45 @@
 import './ProductCard.css'
 import tenis from '../assets/img/Layer-1aa-2.png'
 
-import { Link } from 'react-router-dom';
-export function ProductCard({showDiscount}) {
+import { Link } from 'react-router-dom'
+export function ProductCard({name, image, price, showDiscount, title, priceDiscount}) {
 
   return (
     <>
       
-        <div className='card'>
-          <div className='cardTenis'>
-            {showDiscount && (
-            <div className='discount'>
-              <p>30% OFF</p>
-            </div>)}
-              <div className='tenis'>
-                <img src={tenis} alt="" className='tenisImg' />
-              </div>
-          </div>
-        <Link to={"/produtos"}>
-        <section className='bottom'>
-          <div className='tit1'>
-            <p className='tit1'>Tênis</p>
-          </div>
-          <div className='tit2'>
-              K-Swiss V8 - Masculino
-          </div>
-          <Link to={"/produtos"}>
-            <div className='precos'>
-                <div className='descPreco'>
-                  <p><span>$200</span></p>
+      <div className='card'>
+            <div className='cardTenis'>
+                {showDiscount && (
+                    <div className='discount'>
+                        <p>30% OFF</p>
+                    </div>
+                )}
+                <div className='tenis'>
+                    <img src={image} alt={name} className='tenisImg' />
                 </div>
-            <div className='preco'>
-              <p>$100</p>
             </div>
-            </div>
-          </Link>
-        </section>
-        </Link>
+            <Link to={"/produtos"}>
+                <section className='bottom'>
+                    <div className='tit1'>
+                        <p className='tit1'>{name}</p>
+                    </div>
+                    <div className='tit2'>
+                        <p>{title}</p>
+                    </div>
+                    <Link to={"/produtos"}>
+                        <div className='precos'>
+                            <div className='descPreco'>
+                                <p><span>${priceDiscount}</span></p>
+                            </div>
+                            <div className='preco'>
+                                <p>${price}</p>
+                            </div>
+                        </div>
+                    </Link>
+                </section>
+            </Link>
         </div>
-     
+    
     </>
   )
 }
