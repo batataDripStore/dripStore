37e4a7856907ca1ec1830948444013d9a326f301
Jefferson199809 <jeffersonfortes98@gmail.com>
2024-07-24 13:06:29 -0300

    atualizção da single product

--- src/componentes/Product_ID/ProductId.jsx
+++ src/componentes/Product_ID/ProductId.jsx
@@ -1,21 +1,24 @@
-
+import './ProductId.css'
 import imgTenis from "../../assets/img/image2.png"
+import Avaliacao from "../Avaliacao/Avaliacao"
+import TamanhoProduto from "../TamanhoProduto/TamanhoProduto"
+import CorProduto from "../CorProduto/CorProduto"
 
 export const ProductId = () => {
     return (
-        <div>
-            <img src={imgTenis} alt="" />
-            <h1> Tênis Nike Revolution 6 Next Nature Masculino</h1>
-
-
-            <span>Descrição do Produto</span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi numquam praesentium architecto iste rem corporis reiciendis amet pariatur quaerat exercitationem labore quia quasi laboriosam, laborum natus dolor eum vitae.</p>
-
-
-            
-
-            <button>Comprar</button>
-
+        <div className="main-product">
+            <div>
+                <img src={imgTenis} alt="" />
+            </div>
+            <div className='content-product'>
+                <h1> Tênis Nike Revolution 6 Next Nature Masculino</h1>
+                <Avaliacao/>
+                <span>Descrição do Produto</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi numquam praesentium architecto iste rem corporis reiciendis amet pariatur quaerat exercitationem labore quia quasi laboriosam, laborum natus dolor eum vitae.</p>
+                <TamanhoProduto/>
+                <CorProduto />
+                <button>Comprar</button>
+            </div>
 
         </div>
     )
